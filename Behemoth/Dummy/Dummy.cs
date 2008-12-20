90f7a7a1bd168cbf2930924a465b10278e79d174
Risto Saarelma <risto.saarelma@iki.fi>
2008-12-21 01:01:55 +0200

    Wrote initial readme, added license, wrote dummy library and example app.

---
+++ Behemoth/Dummy/Dummy.cs
@@ -0,0 +1,26 @@
+
+using System;
+namespace Behemoth.Dummy
+{
+
+  /// <summary>
+  /// An example Behemoth library class.
+  /// </summary>
+  public static class Dummy
+  {
+    public static void HelloWorld()
+    {
+      System.Console.WriteLine("Hello, world!");
+    }
+
+    /// <summary>
+    /// The factorial function.
+    /// </summary>
+    public static int Factorial(int n)
+    {
+    }
+
+  }
+
+
+}
---
+++ examples/DummyApp/DummyApp.cs
@@ -0,0 +1,12 @@
+using Behemoth.Dummy;
+
+namespace DummyApp
+{
+  public class DummyApp
+  {
+    public static void Main()
+    {
+      Dummy.HelloWorld();
+    }
+  }
+}
